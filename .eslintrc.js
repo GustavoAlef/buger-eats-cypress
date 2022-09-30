627017d1c9dee6f3113070ba3ac3e758622783b5
Gustavo Alef <gustavo.alef@seedz.ag>
2022-09-30 08:42:21 -0300

    ajustando config inicial

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:cypress/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        quotes: ["error", "double"],
+        indent: ["error", 4],
+        "comma-spacing": ["error", {before: false, after: true}]
+
+    }
+}
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe("home page", () => {
+    it("app deve estar online", () => {
+        cy.visit("https://buger-eats.vercel.app")
+        cy.get("h1").should("have.text", "Seja um parceiro entregador pela Buger Eat")
+
+
+
+
+    })
+})
